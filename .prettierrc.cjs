706e24e29a15e2ae13733471ffb0d403ed13a546
Bryan Sammon <bryansammon@yahoo.com>
2023-04-16 13:39:14 -0500

    add project config

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSpacing: true,
+  trailingComma: 'all',
+  semi: true,
+  editorconfig: true,
+  singleQuote: true,
+  importOrder: ['^[~./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: [],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-import path from 'path';
+import path from 'path'
 
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default {
   entry: './src/index.ts',
